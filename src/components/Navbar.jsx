5e132510c46ad3b40b7736d49f3fa5c1aa39e478
Rita <ritasaad262@gmail.com>
2026-05-27 22:17:57 +0300

    first

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1>My Task Manager</h1>
+
+      <p>Organize your daily tasks easily</p>
+    </nav>
+  )
+}
+
+export default Navbar

7ce9a97246baac09d7acebf491150d1e080a30a5
Rita <ritasaad262@gmail.com>
2026-05-31 23:26:39 +0300

    fourth

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 function Navbar() {
   return (
     <nav className="navbar">
-      <h1>My Task Manager</h1>
+      <h1>Task Manager</h1>
 
       <p>Organize your daily tasks easily</p>
     </nav>

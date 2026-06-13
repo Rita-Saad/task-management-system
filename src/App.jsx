38dc37ffe6d9ceafa1ba0f7bf807112350610c70
Rita <ritasaad262@gmail.com>
2026-06-13 17:17:12 +0300

    14

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import Navbar from "./components/Navbar"
 import Dashboard from "./pages/Dashboard"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
+import { Routes, Route } from "react-router-dom"
+
 function App() {
   return (
     
     <div>
       <Navbar />
-      <Dashboard />
+
+      
+        <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom"
 function Navbar() {
   return (
     <nav className="navbar">
       <h1>Task Manager</h1>
 
       <p>Organize your daily tasks easily</p>
+      <div>
+      <Link to="/">Dashboard</Link>
+
+      <Link to="/about">About</Link>
+
+      <Link to="/profile">Profile</Link>
+      </div>
+
     </nav>
   )
 }
